5d2279f4a05dc649b8e6b7fadbf575208bf48b64
Wei Ke <weik@andrew.cmu.edu>
2019-05-13 13:22:48 -0400

    fixing merge problem

--- train.py
+++ train.py
@@ -23,13 +23,11 @@ def train_epoch(epoch, data_loader, model, criterion, optimizer, opt,
         data_time.update(time.time() - end_time)
 
         if not opt.no_cuda:
-<<<<<<< HEAD
+
             targets_vid = targets.cuda(non_blocking=True)
             targets_img = torch.cat((targets, targets, targets, targets, targets, targets, targets, targets), dim=0)
             targets_img = targets_img.cuda(non_blocking=True)
-=======
-            targets = targets.cuda(non_blocking=True)
->>>>>>> 90119755c114b12d9538c240f4b103a48c9a6c54
+            
         inputs = Variable(inputs)
         targets_vid = Variable(targets_vid)
         targets_img = Variable(targets_img)
@@ -38,7 +36,6 @@ def train_epoch(epoch, data_loader, model, criterion, optimizer, opt,
         loss_img = criterion(outputs[0], targets_img)
         loss = loss_vid + loss_img
         acc = calculate_accuracy(outputs[1], targets_vid)
-
         losses.update(loss.data.cpu(), inputs.size(0))
         accuracies.update(acc, inputs.size(0))
 
